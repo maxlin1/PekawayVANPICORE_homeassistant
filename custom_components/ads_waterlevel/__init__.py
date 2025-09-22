88023a8a7b4faa64a548b925698e8be2414b63ef
Max Lindner <maxlin1@users.noreply.github.com>
2025-09-22 19:51:08 +0200

    Bug Fix Wassermessung __init__.py

--- custom_components/ads_waterlevel/__init__.py
+++ custom_components/ads_waterlevel/__init__.py
@@ -1,23 +1,8 @@
-import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.discovery import load_platform
-
-_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "ads_waterlevel"
 
 def setup(hass: HomeAssistant, config: dict):
-    """Set up the ADS1115 Water Level component."""
-    if DOMAIN not in config:
-        return True
-
-    # Lade die Sensor-Plattform gemäß den Konfigurationseinstellungen
-    sensor_configs = config[DOMAIN].get("sensors", [])
-    for sensor_config in sensor_configs:
-        hass.data.setdefault(DOMAIN, []).append(sensor_config)
-
-    # Lade die Sensor-Platform (sensor.py) für ads_waterlevel
-    load_platform(hass, "sensor", DOMAIN, {}, config)
-
-    _LOGGER.info("ADS1115 Water Level component geladen")
+    # Plattform-basierte Einrichtung
     return True
